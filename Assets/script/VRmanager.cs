3cf4fe139779540461b5a056b5fd7aabb7d34994
BzLOVEman <segareyarou@yahoo.co.jp>
2019-03-11 15:33:08 +0900

    Marge Finished
    Marge Finished ,Unity collab and Github

---
+++ Assets/script/VRmanager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.XR;
+
+public class VRmanager : MonoBehaviour {
+
+	void Start () {
+        if (SceneManager.GetActiveScene().name=="debug"             ||
+            SceneManager.GetActiveScene().name=="game"              ||
+            SceneManager.GetActiveScene().name=="hard"              ||
+            SceneManager.GetActiveScene().name=="StaffRole"         ||
+            SceneManager.GetActiveScene().name=="StaffRoleCreditOnly") {
+            XRSettings.enabled=true;
+        } else {
+            XRSettings.enabled=false;
+        }
+        if (XRSettings.enabled) {
+            InputTracking.Recenter();
+            InputTracking.disablePositionalTracking=true;
+            XRDevice.SetTrackingSpaceType(TrackingSpaceType.Stationary);
+        }
+    }
+
+    void Update () {
+        this.gameObject.transform.localPosition=Vector3.zero;
+    }
+}
